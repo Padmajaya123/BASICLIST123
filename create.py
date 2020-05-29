33f102c035a9390c4a2d721a0ddbc8dc0132a032
Rekha <saipadmarekha@gmail.com>
2020-05-29 15:55:44 +0530

    Initial commit

---
+++ create.py
@@ -0,0 +1,10 @@
+List = []
+print("Blank List: ")
+print(List)
+List = ["Geeks", "For", "Geeks"]
+print("\nList Items: ")
+print(List[0])
+print(List[2])
+List = [['Geeks', 'For'] , ['Geeks']]
+print("\nMulti-Dimensional List: ")
+print(List)
